d8dd32b96968e4440256f73c8578d11010c41e35
Aleisi <86322128+Aleisi@users.noreply.github.com>
2021-10-26 17:29:44 -0500

    adding node_modules into my-app file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './App.css';
 
-export default function App() {
-    return(<div>HOLA MUNDAAAA</div>);
+// export default function App() {
+//     return(<div>HOLA MUNDAAAA</div>);
+// }
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from './components/Home'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+          <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+  );
 }
 
-
+export default App;
 
 
 
